135c3f84e871aedd8c579a244eed5d499b77aded
77tonbi88 <40909739+77tonbi88@users.noreply.github.com>
2020-08-16 09:12:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,46 @@
-from discord.ext import commands
+import discord
+import time
+import random
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.content == "!kanchome":
+        try:
+            voich = await discord.VoiceChannel.connect(message.author.voice.channel)
+            voice_client = message.guild.voice_client
+            num = random.randint(1, 5)
+            source = discord.FFmpegPCMAudio(str(num)+".mp3")
+            voice_client.play(source)
+            time.sleep(2)
+            await voich.disconnect()
+        except AttributeError:
+            num = random.randint(1, 5)
+            if num == 1:
+                await message.channel.send("………なんのつもりだい？ " + message.author.name + "。")
+            elif num == 2:
+                await message.channel.send("ふわあ…" + message.author.name + "、今日はもう寝ないかい？僕もう眠いんだ…")
+            elif num == 3:
+                await message.channel.send("おはよう" + message.author.name + "！" + message.author.name + "は今朝もカッコイイぜ！")
+            elif num == 4:
+                await message.channel.send( message.author.name + "ーー！！　大事なコンサートほっぽりだして何やってんだよー！")
+            elif num == 5:
+                await message.channel.send("フゥ…やっとミラノに帰れるんだ…　あれ？あんなところにお菓子が落ちてるぞ。もったいないなあ…　あれ？船が動きだして…　わーーー" + message.author.name + "ーーーー!!!")
 
 
-bot.run(token)
+client.run(token)
