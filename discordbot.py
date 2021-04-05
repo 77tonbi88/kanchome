54cb25ccc0803068119036588677f7a3a852a5ef
KODERA <59335458+KDR121@users.noreply.github.com>
2021-04-05 10:43:49 +0900

    add !delete command

--- discordbot.py
+++ discordbot.py
@@ -159,5 +159,36 @@ async def on_message(message):
             await message.channel.send("？？「デブでしか抜けん」")
         elif num == 2:
             await message.channel.send("？？「中学生時代のあだ名は歩く18禁」")
+    if client.user != message.author:
+        # 削除コマンド
+        if message.content.startswith("!delchat "):
+            #役職比較
+            if discord.utils.get(message.author.roles, name="admin"):
+                # メッセージを格納
+                delcmd = message.content
+                # 入力メッセージのリスト化
+                delcmd_ = delcmd.split()
+                # 入力メッセージのint化
+                delcmd_int = int(delcmd_[1])
+                # 入力メッセージの単語数
+                delcmd_c = len(delcmd_)
+                if delcmd_c == 2 and delcmd_int <= 50 and delcmd_int > 1:
+                    # メッセージ取得
+                    msgs = [msg async for msg in client.logs_from(message.channel, limit=(delcmd_int+1))]
+                    await client.delete_messages(msgs)
+                    delmsg = await client.send_message(message.channel, '削除が完了しました')
+                    await sleep(5)
+                    await client.delete_message(delmsg)
+                else:
+                    # エラーメッセージを送ります
+                    delmsg = await client.send_message(message.channel, "コマンドが間違っています。[!delchat *] *:2～50")
+                    await sleep(5)
+                    await client.delete_message(delmsg)
+                    
+            else:
+                # エラーメッセージを送ります
+                delmsg = await client.send_message(message.channel, "admin権限がありません。")
+                await sleep(5)
+                await client.delete_message(delmsg)
 
 client.run(token)
