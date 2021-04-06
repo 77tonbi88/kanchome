7ffee7b94e2836ba59cfa9581352a4102454b4aa
KODERA <59335458+KDR121@users.noreply.github.com>
2021-04-06 19:32:23 +0900

    fix

--- discordbot.py
+++ discordbot.py
@@ -114,7 +114,7 @@ async def on_message(message):
     if message.content == "!ono":
         await message.channel.send("小野...ヒデアキ")
     if message.content == "!kodera":
-        await message.channel.send("コデラ...5")
+        await message.channel.send("コデラ...6")
     if message.content == "!katayama1":
         try:
             voich = await discord.VoiceChannel.connect(message.author.voice.channel)
@@ -159,7 +159,8 @@ async def on_message(message):
             await message.channel.send("？？「デブでしか抜けん」")
         elif num == 2:
             await message.channel.send("？？「中学生時代のあだ名は歩く18禁」")
-    if message.content.startwith("!delchat "):
+    if message.content.startwith == '!delchat ':
+        await message.channel.send("通ってます！")
         try:
             if discord.utils.get(message.author.roles, name="admin"):
                 delcmd = message.content
@@ -169,9 +170,9 @@ async def on_message(message):
                 if delcmd_c == 2 and delcmd_int <= 50 and delcmd_int > 1:
                     msgs = [msg async for msg in client.logs_from(message.channel, limit=(delcmd_int + 1 ))]
                     await client.delete_message(msgs)
-                await message.channel.send("削除が完了しました5")
+                await message.channel.send("削除が完了しました6")
             else:
-                await message.channel.send("権限がありません5")
+                await message.channel.send("権限がありません6")
         except AttributeError:
             await message.channel.send("うまくいかなかったよ～")
 
