c9836284f3fd3e92784a220d4012487a88c39d6c
KODERA <59335458+KDR121@users.noreply.github.com>
2021-06-02 12:17:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -179,12 +179,10 @@ async def on_message(message):
         except AttributeError:
             await message.channel.send("うまくいかなかったよ～")
     if message.content == "!test":
-        await message.channel.send("7")
+        await message.channel.send("8")
     content_champ = message.content
     kanchome_text = ""
-    await message.channel.send("1" + kanchome_text)
     kanchome_text = lol_champion.change_champion_name(content_champ)
-    await message.channel.send(kanchome_text)
     if kanchome_text != "":
         await message.channel.send(kanchome_text)
 
